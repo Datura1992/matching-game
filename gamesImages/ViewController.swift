db6b8804b687c69b4c3b90e98d779e463cf23f05
Александра Иванова <aleksandraivanova@MacBook-Air-Aleksandra.local>
2023-06-12 15:34:16 +0600

    first commit

---
+++ gamesImages/ViewController.swift
@@ -0,0 +1,164 @@
+//
+//  ViewController.swift
+//  gamesImages
+//
+//  Created by Александра Иванова on 28.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @IBOutlet weak var movesLabel: UILabel!
+    
+    var state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    
+    var images = ["bird","buket","butterfly","ciren", "flowerRomashka","rose", "sakura", "vasa", "bird","buket","butterfly","ciren", "flowerRomashka","rose", "sakura", "vasa"]
+    
+    var winState = [[0,8], [1, 9], [2, 10], [3, 11], [4, 12], [5, 13], [6, 14], [7, 15]]
+    
+    var isFreezPlayer = false
+    var isTimeRunning = false
+    var timer = Timer()
+    var time = 0
+    var moveCount = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        cleanGame()
+        
+    }
+     @objc func countTime() {
+        
+        time += 1
+        timerLabel.text = "Time: \(timeToString(intTime: time))"
+    }
+    
+    func timeToString(intTime: Int) -> String {
+        let seconds = intTime % 60
+        let minutes = (intTime / 60) % 60
+        let hours = intTime / 3600
+        
+        return String(format: "%0.2d:%0.2d:%0.2d", hours, minutes, seconds)
+    }
+    
+    @objc func startTime() {
+        if isTimeRunning {
+            return
+        }
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countTime), userInfo: nil, repeats: true)
+    }
+       
+        @IBAction func buttonTouched(_ sender: UIButton) {
+            
+            startTime()
+            
+            if state[sender.tag - 1] != 0 || isFreezPlayer {
+                return
+            }
+            state[sender.tag - 1] = 1
+            
+            
+            sender.setBackgroundImage(UIImage(named: images[sender.tag - 1]), for: .normal)
+            
+            
+            var count = 0
+            
+            for item in state {
+                if item == 1 {
+                    count += 1
+                    
+                }
+            }
+            
+            if count == 2 {
+                
+                moveCount += 1
+                movesLabel.text = "Moves: \(moveCount)"
+                
+                isFreezPlayer = true
+                for winArray in winState {
+                    if state[winArray[0]] == state[winArray[1]] &&  state[winArray[0]] == 1 {
+                        state[winArray[0]] = 2
+                        state[winArray[1]] = 2
+                        isFreezPlayer = false
+                        
+                    }
+                    
+                }
+                
+                if isFreezPlayer {
+                    Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(clear), userInfo: nil, repeats: false)
+                }
+                
+            }
+            
+            var countWiner = 0
+            
+            for item in state {
+                if item == 2 {
+                    countWiner += 1
+                }
+            }
+            
+            if countWiner == 16 {
+                
+                timer.invalidate()
+                let alert = UIAlertController(title: "Congratulations!", message: "You win!", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "Ok", style: .default,  handler: { _ in
+                    self.cleanGame()
+                }))
+                
+                present(alert, animated: true)
+            }
+            
+        }
+        @objc func clear() {
+            
+            time = 0
+            moveCount = 0
+            
+            timerLabel.text = "Time: 00:00:00"
+            movesLabel.text = "Moves: 0"
+            
+            isTimeRunning = false
+            
+            
+            isFreezPlayer = false
+            
+            for i in 0...15 {
+                if state[i] == 1 {
+                    state[i] = 0
+                    let button = view.viewWithTag(i + 1) as!UIButton; button.setBackgroundImage(nil, for: .normal)
+                }
+                
+            }
+            
+        }
+        
+        func cleanGame() {
+            for i in 0...15 {
+                state[i] = 0
+                let button = view.viewWithTag(i + 1) as!UIButton
+                button.setBackgroundImage(nil, for: .normal)
+            }
+            images.shuffle()
+            winState.removeAll()
+            
+            for i in 0...15 {
+                for j in 0...15 {
+                    if images[i] == images[j]  && i != j {
+                        winState.append([i, j])
+                        break
+                    }
+                }
+            }
+        }
+    }
+
+
